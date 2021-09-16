3c3b0e37f36512f07ac38ce591c0fa4d1939464e
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-09-15 23:08:29 -0700

    serialization???

--- source/TaskManager.cs
+++ source/TaskManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using YamlDotNet.Serialization;
 
 namespace cards
 {
     public static class TaskManager
     {
+        public const string TaskPath = @"C:\Users\dninemfive\Documents\notes\md\tasks2.yml";
         public static List<Task> AllTasks = new List<Task>();
         public static List<Task> TasksWhichAre(TaskState s) => AllTasks.Where(x => x.State == s).ToList();
         public static List<Task> InactiveTasks => TasksWhichAre(TaskState.INACTIVE);
@@ -20,5 +22,16 @@ namespace cards
                 return selectable[ind];
             }
         }
+        public static void LoadTasks()
+        {
+            string yml = System.IO.File.ReadAllText(TaskPath);
+            Deserializer deserializer = new Deserializer();
+            TaskList list = deserializer.Deserialize<TaskList>(yml);
+            foreach(Task t in list.tasks)
+            {
+                AllTasks.Add(t);
+            }
+        }
     }
+    public class TaskList { public List<Task> tasks;  }
 }
