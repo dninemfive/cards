b0d97ef66833f33ce49200847b24d446d15fead6
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-09-15 22:00:06 -0700

    main window stuff

--- source/Task.cs
+++ source/Task.cs
@@ -8,23 +8,38 @@ namespace cards
 {
     public class Task
     {
+        #region variables
         public string Title = null, Content = null;
         public TaskState State = TaskState.INACTIVE;
-        public DateTime StartTime, Deadline, SatisfiedUntil;
-        public TimeSpan TimeRemaining => Deadline - DateTime.Now;
-        public bool TimedOut => TimeRemaining <= TimeSpan.Zero;
+
+        #region time
+        public DateTime Deadline;
+        public DateTime? SatisfiedUntil = null;
         public TimeSpan? PredictedDuration = null;
         public TimeSpan? FinalDuration = null;
-        public virtual int Weight { get; set; }
+
+        #region derived
+        public TimeSpan TimeRemaining => Deadline - DateTime.Now;
+        public bool TimedOut => TimeRemaining <= TimeSpan.Zero;
+        #endregion derived
+
+        #endregion time
+
+        public int Weight { get; set; }
         public Color Color = Color.Gray;
         public int TimesRepeated = 0, TimesToRepeat = -1;
         public List<string> tags = new List<string>();
-        public List<Task> Subtasks = new List<Task>();
-        public bool IsSupertask => Subtasks.Count == 0;
-        public List<Task> Supertasks = new List<Task>();
-        public bool IsSubtask => Supertasks.Count == 0;
+        #region metatasks
+        public List<Task> Subtasks = new List<Task>();        
+        public List<Task> Supertasks = new List<Task>();        
         public List<Task> Prerequisites = new List<Task>();
+        #region derived
+        public bool IsSupertask => Subtasks.Count == 0;
+        public bool IsSubtask => Supertasks.Count == 0;
         public int PrerequisitesFulfilled => Prerequisites.Where(x => x.State == TaskState.COMPLETE || x.State == TaskState.SATISFIED).ToList().Count;
+        #endregion derived
+        #endregion metatasks
+        #endregion variables
         public bool CanBeRandomlySelected
         {
             get
@@ -32,7 +47,7 @@ namespace cards
                 return State == TaskState.INACTIVE && !IsSupertask && PrerequisitesFulfilled == Prerequisites.Count;
             }
         }
-        public static Task Empty;
+        public static readonly Task Empty;
     }
     public enum TaskState
     {
